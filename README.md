dee2105dae58b0c599870bffbbffb4701349e972
Son Tran <sontn1309@gmail.com>
2023-10-18 21:21:31 +0700

    update readme

--- README.md
+++ README.md
@@ -10,5 +10,10 @@ awslocal lambda create-function \
 --handler index.handler \
 --role arn:aws:iam::000000000000:role/lambda-role
 
-    awslocal lambda create-event-source-mapping --function-name handle-sqs --batch-size 10 \
-    --event-source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue
+awslocal lambda create-event-source-mapping --function-name handle-sqs --batch-size 10 \
+--event-source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue
+
+awslocal lambda create-event-source-mapping --function-name handle-sqs --batch-size 10 \
+--event-source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue
+
+
--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 exports.handler = async (event) => {
-    let body = JSON.parse(event.body)
 
-    const response = {
-        statusCode: 200,
-        body
-    };
     console.log('Lambda');
-    console.log(response);
+    console.log(event);
     return response;
 };
-
-awslocal lambda create-function \
-    --function-name handle-sqs \
-    --runtime nodejs18.x \
-    --zip-file fileb://function.zip \
-    --handler index.handler \
-    --role arn:aws:iam::000000000000:role/lambda-role
-
-    awslocal lambda create-event-source-mapping --function-name handle-sqs --batch-size 10 \
-    --event-source-arn arn:aws:sqs:us-east-1:000000000000:localstack-queue
 
